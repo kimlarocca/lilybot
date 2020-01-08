1f20af815272a2c6a8d913784fc6531c8b7ce48e
Kim LaRocca <kimlarocca@me.com>
2020-01-08 11:47:17 -0500

    added the name conversation

---
+++ features/conversations.js
@@ -0,0 +1,25 @@
+/**
+ * This module demonstrates the use of the typing indicator in a conversation, and when using bot.reply
+ * Tell your bot "typing dialog" or "typing reply" to see this in action.
+ */
+const {BotkitConversation} = require("botkit");
+
+module.exports = function (controller) {
+
+    let nameConvo = new BotkitConversation('nameConvo', controller);
+
+    nameConvo.say('My name is Lilybot');
+
+    // ask a question, store the response in 'name'
+    nameConvo.ask('What is your name?', async (response, nameConvo, bot) => {
+        // do something?
+    }, 'name');
+
+    nameConvo.say('Hi, {{vars.name}}!');
+
+    controller.addDialog(nameConvo);
+
+    controller.hears(['what is your name', 'what\'s your name', 'whats your name'], 'message', async (bot, message) => {
+        await bot.beginDialog('nameConvo');
+    });
+};
